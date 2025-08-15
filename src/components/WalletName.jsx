4dcdca8426c9d2d9e3df3967e2b41075eca7b475
Ezpeasy0 <158174291+Ezpeasy0@users.noreply.github.com>
2025-08-15 14:19:40 +0700

    Update name

--- src/components/WalletName.jsx
+++ src/components/WalletName.jsx
@@ -7,7 +7,7 @@ function WalletName() {
             <p class="p-[10px] text-[16px] text-black">Wallet Name</p>
             <div className="flex items-center justify-left h-full 
                             w-[362px] h-[40px] bg-[#E7EBEE] shadow-[0_4px_6px_rgba(0,0,0,0.2)] rounded-lg">
-                <p class="m-[20px] text-[16px] text-[#707376]">Name Your Wallet |</p>
+                <p class="m-[20px] text-[16px] text-[#707376]">Name Your Walle |</p>
             </div>
             
         </div>
