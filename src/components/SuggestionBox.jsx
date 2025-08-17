5a2465fb1a70686449c47b42070c3a914b2d43bf
Ohmar9121 <102794912+Ohmar9121@users.noreply.github.com>
2025-08-17 19:29:47 +0700

    Finished Suggestion Box

--- src/components/SuggestionBox.jsx
+++ src/components/SuggestionBox.jsx
@@ -4,11 +4,13 @@ function SuggestionBox(){
     return (
     <>
     <div className="flex justify-center items-center w-322 h-137
-    box-content border-1 p-4 rounded-xl bg-gray-100">
-       <h1 className="absolute top-0 left-0 pl-20 pt-10 text-6xl">
+    box-content border-1 p-4 rounded-4xl bg-gray-100">
+       <h1 className="absolute top-0 left-0 pl-20 pt-15 text-8xl">
         Suggestions
        </h1>
-       
+       <h2 className="text-[75px] pl-15 pt-30 pr-60 tracking-tighter leading-snug ">
+        Change to spending 100 baht per day, this month you will save 280 baht.
+       </h2>
     </div>
     </>
     );
