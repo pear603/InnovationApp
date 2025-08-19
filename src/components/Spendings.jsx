622b7b2550007b3c5753ea4373e158822215b19b
Ohmar9121 <102794912+Ohmar9121@users.noreply.github.com>
2025-08-19 22:16:17 +0700

    Added Notes and fixed Colors

--- src/components/Spendings.jsx
+++ src/components/Spendings.jsx
@@ -6,7 +6,7 @@ function Spendings() {
         <div class="relative group">
         <div>
             <label for="first_name" class="block mb-1 text-[45px] text-gray-900 text-black">Spending</label>
-            <input type="text" id="first_name" class="focus:outline-none focus:ring-0 focus:border-gray-500 pl-10 bg-gray-50 border-2 rounded-[25px] text-gray-900 text-[45px] rounded-[20px] w-173 h-30 p-2.5 dark:bg-white-700 text-black" placeholder="Type Spending" required />
+            <input type="text" id="first_name" class="focus:outline-none focus:ring-0 focus:border-gray-500 pl-10 bg-[#E7EBEE] drop-shadow-2xl border-0 rounded-[25px] text-gray-900 text-[45px] rounded-[20px] w-362 h-30 p-2.5 dark:bg-white-700 text-black" placeholder="Type Spending |" required />
         </div>
         </div>
         </>
--- src/pages/InWallet.jsx
+++ src/pages/InWallet.jsx
@@ -2,6 +2,7 @@ import GoodToKnow from "../components/GoodToKnow";
 import SelectTag from "../components/SelectTag";
 import SuggestionBox from "../components/SuggestionBox";
 import Spendings from "../components/Spendings";
+import Notes from "../components/Notes";
 import "../tailwind.css";
 function InWallet() {
     return(
@@ -10,6 +11,7 @@ function InWallet() {
             <GoodToKnow/>
             <SelectTag/>
             <Spendings/>
+            <Notes/>
         </div>
     );
 }
