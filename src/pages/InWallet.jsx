255097837f14b25d4748cdb58b22fc71b95282b0
Ohmar9121 <102794912+Ohmar9121@users.noreply.github.com>
2025-08-21 20:05:07 +0700

    Updated imports (forgot)
    Updated imports for file movement

--- src/pages/InWallet.jsx
+++ src/pages/InWallet.jsx
@@ -1,11 +1,11 @@
-import GoodToKnow from "../components/GoodToKnow";
-import SelectTag from "../components/SelectTag";
-import SuggestionBox from "../components/SuggestionBox";
-import Spendings from "../components/Spendings";
-import Notes from "../components/Notes";
-import Insert from "../components/Insert";
-import PieStats from "../components/PieStats";
-import BarGraph from "../components/BarGraph";
+import GoodToKnow from "../components/Ohma/GoodToKnow";
+import SelectTag from "../components/Ohma/SelectTag";
+import SuggestionBox from "../components/Ohma/SuggestionBox";
+import Spendings from "../components/Ohma/Spendings";
+import Notes from "../components/Ohma/Notes";
+import Insert from "../components/Ohma/Insert";
+import PieStats from "../components/Ohma/PieStats";
+import BarGraph from "../components/Ohma/BarGraph";
 import "../tailwind.css";
 
 function InWallet() {
