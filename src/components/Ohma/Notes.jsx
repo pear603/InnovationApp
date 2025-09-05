aca96372d6d61676981337eee09c83e9c7ce7b78
Ohmar9121 <102794912+Ohmar9121@users.noreply.github.com>
2025-09-05 13:11:12 +0700

    Finished AddIncomeTrans

--- src/components/Ohma/Notes.jsx
+++ src/components/Ohma/Notes.jsx
@@ -5,8 +5,8 @@ function Notes() {
         <>
         <div class="relative group">
         <div>
-            <label for="first_name" class="block mb-1 text-[45px] text-gray-900 text-black">Notes</label>
-            <input type="text" id="first_name" class="focus:outline-none focus:ring-0 focus:border-gray-500 pl-10 bg-[#E7EBEE] drop-shadow-2xl border-0 rounded-[25px] text-gray-900 text-[45px] rounded-[20px] w-551 h-30 p-2.5 dark:bg-white-700 text-black" placeholder="Note |" required />
+            <label for="first_name" class="block mb-1 mt-1 text-[15px] text-gray-900 text-black">Notes</label>
+            <input type="text" id="first_name" class="focus:outline-none focus:ring-0 focus:border-gray-500 border border-black/15 pl-4 pb-1 bg-[#E7EBEE] drop-shadow-lg rounded-[6px] text-gray-900 text-[14px] w-full h-8 dark:bg-white-700 text-black" placeholder="Note |" required />
         </div>
         </div>
         </>
--- src/components/Ohma/SelectTag.jsx
+++ src/components/Ohma/SelectTag.jsx
@@ -4,18 +4,18 @@ function SelectTag() {
     return (
         <>
         <div class="relative group">
-        <h1 class="flex flex-col text-[45px]">
-            Select Tags
-        <button class="flex flex-row text-[45px] justify-left bg-white-500 w-173 h-30 border-1 rounded-[25px] text-black rounded hover:bg-blue-400 focus:outline-none pl-10 pt-5 text-opacity-[0]">
-            Dropdown
-            <span class="pl-95 pt-3 text-[30px] font-bold"> V</span>
-        </button>
-        <div class=" hidden group-hover:block bg-white shadow-lg rounded mt-1 w-173">
-            <a href="#" class="block px-4 py-2 pl-20 text-gray-800 hover:bg-blue-400 hover:text-white">Tag 1</a>
-            <a href="#" class="block px-4 py-2 pl-20 text-gray-800 hover:bg-blue-400 hover:text-white">Tag 2</a>
-            <a href="#" class="block px-4 py-2 pl-20 text-gray-800 hover:bg-blue-400 hover:text-white">Tag 3</a>
-        </div>
-        </h1>
+            <h1 class="flex flex-col text-[15px] font-[400]">
+                Select Tag
+            <button class="flex flex-row text-[14px] drop-shadow-lg justify-left bg-white-500 w-full h-8 rounded-[8px] text-black rounded border border-black/15 hover:bg-blue-400 focus:outline-none mt-[4px] pl-3 pt-[4px] text-opacity-[0]">
+                Tags
+                <span class="pt-1 pl-35 text-[10px]"> V</span>
+            </button>
+            <div class=" absolute z-50 hidden group-hover:block bg-white shadow-lg rounded-[8px] border border-black/25 mt-15 w-40">
+                <a href="#" class="block px-4 py-2 pl-3 text-gray-800 rounded-[4px] hover:bg-blue-400 hover:text-white">Tag 1</a>
+                <a href="#" class="block px-4 py-2 pl-3 text-gray-800 rounded-[4px] hover:bg-blue-400 hover:text-white">Tag 2</a>
+                <a href="#" class="block px-4 py-2 pl-3 text-gray-800 rounded-[4px] hover:bg-blue-400 hover:text-white">Tag 3</a>
+            </div>
+            </h1>
         </div>
         </>
     );
