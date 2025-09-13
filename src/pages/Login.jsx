1424773ecf67e2a8425d2572cb92874c91365fe1
Ohmar9121 <102794912+Ohmar9121@users.noreply.github.com>
2025-09-13 20:51:10 +0700

    Create Login.jsx

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import "../tailwind.css";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+function Login() {
+    return (
+        <>
+        </>
+    );
+}
+
+export default Login;
