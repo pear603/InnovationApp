c1d052b8c15ff4a23c8e64cdca9bace51e95e078
Ezpeasy0 <158174291+Ezpeasy0@users.noreply.github.com>
2025-09-25 23:15:47 +0700

    Link to Page add-Ex/In Trans

--- src/pages/ExpenseWalletDetails.jsx
+++ src/pages/ExpenseWalletDetails.jsx
@@ -8,32 +8,39 @@ import PieStats from "../components/Ohma/PieStats";
 import BarGraph from "../components/Ohma/BarGraph";
 import "../tailwind.css";
 import BalanceLeft from "../components/BalanceLeft";
-import ExpenseButton from "../components/ExpenseBtn";
+import Expense from "../components/Ohma/Expense";
 import Transaction from "../components/Ohma/Transaction";
+import { useNavigate } from "react-router-dom";
+
 
 function ExpenseWalletDetails() {
+  const navigate = useNavigate(); 
+  const handleExpenseClick = () => {
+    navigate("/ExpenseTx"); 
+  };
+
   return (
     <div className="w-screen h-screen flex flex-row items-start justify-center bg-[#E2EFF3]">
       <div className=" ml-[76px] flex flex-col items-start space-y-2 bg-transparent">
         {/*Wallet*/}
         <h1 className="text-[32px]">Wallet Name</h1>
         {/*Current Balance*/}
-        <dix className="w-[739px] h-[197px]">
+        <div className="w-[739px] h-[197px]">
             <BalanceLeft/>
-        </dix>
+        </div>
         {/*Insert,Expense*/}
         <div className="mt-2 flex flex-row w-full max-w-[739px] h-[50px]">
             <div className="  w-full h-full gap-2 w-full">
-                <ExpenseButton/>
+                <Expense onClick={handleExpenseClick} />
             </div>
         </div>
         {/*Statistics box*/}
         <div className="flex flex-col bg-white w-[741px] h-[691px] border border-black/25 rounded-[10px]">
             <div className="mt-5 ml-10 text-[24px] font-normal">Statistics</div>
             <div className="grid grid-cols-2 w-full h-[291px] mt-4 gap-5">
-                <dix className="ml-10 mr-8 mb-5">
+                <div className="ml-10 mr-8 mb-5">
                     <PieStats/>
-                </dix>
+                </div>
                 <div className="w-[331px] h-[291px] flex flex-col gap-5">
                     <SuggestionBox/>
                     <GoodToKnow/>
@@ -45,14 +52,14 @@ function ExpenseWalletDetails() {
         </div>
       </div>
       {/*Transaction box*/}
-      <dix className="w-[523px] h-[962px] bg-white mt-14 ml-3 border border-black/25 rounded-[10px]">
+      <div className="w-[523px] h-[962px] bg-white mt-14 ml-3 border border-black/25 rounded-[10px]">
           <div className="mt-5 ml-10 mb-10 text-[29px] font-normal">Transaction</div>
           <div className="ml-10">
               <Transaction/>
               <Transaction/>
               <Transaction/>
           </div>
-      </dix>
+      </div>
     </div>
   );
 }
