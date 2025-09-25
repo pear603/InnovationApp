0409407f0c4c7406cd7424519010872988aa4de2
Ezpeasy0 <158174291+Ezpeasy0@users.noreply.github.com>
2025-09-25 23:13:57 +0700

    Add onClick

--- src/components/Ohma/Insert.jsx
+++ src/components/Ohma/Insert.jsx
@@ -1,9 +1,15 @@
 import "../../tailwind.css";
 
-function Insert() {
+function Insert({ onClick }) { 
   return (
     <div className="relative group w-full h-full">
-      <button type="button" className="w-full h-full flex flex-col justify-center text-[20px] focus:outline-none text-black  bg-[#9AD24B] focus:ring-1 rounded-[10px] text-sm hover:bg-[#ACDA6C] active:bg-[#9AD24B]">Insert</button>
+      <button
+        type="button"
+        onClick={onClick} 
+        className="w-full h-full flex flex-col justify-center text-[20px] focus:outline-none text-black bg-[#9AD24B] focus:ring-1 rounded-[10px] text-sm hover:bg-[#ACDA6C] active:bg-[#9AD24B]"
+      >
+        Insert
+      </button>
     </div>
   );
 }
